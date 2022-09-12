4ed7ae76d207260766e7891a77efc75767442470
Clovis Fong <fongclovis@hotmail.com>
2022-09-12 10:57:14 +0800

    connect to cyclic

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
+// Depencies
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require("morgan");
+const holiday = require('./models/Holidays')
 
 
-
+//Configuration
 const MONGO_URI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/holiday'
 mongoose.connect(MONGO_URI, {}, ()=> { console.log('connected to mongoDB')})
 const app = express()
+const port = process.env.PORT ?? 3000
 
-
+//Middleware
 app.use(express.json())
+app.use(morgan("dev"));
 
+
+app.get('/',(req,res) => {
+    res.send('testing from backend')
+})
+
+app.get('/holidays', (req,res) => {
+    
+})
+
+
+
+app.listen(port, ()=>{
+    console.log('Express listening to ' + port)
+})
 
