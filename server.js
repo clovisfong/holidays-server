06ca1193497f58c8d66f12501c9efa69b7b913c7
Clovis Fong <fongclovis@hotmail.com>
2022-09-12 14:38:42 +0800

    update

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ const holiday = require('./models/Holidays')
 
 //Configuration
 const MONGO_URI = process.env.MONGO_URI ?? 'mongodb://localhost:27017/holiday'
-mongoose.connect(MONGO_URI, {}, ()=> { console.log('connected to mongoDB')})
 const app = express()
 const port = process.env.PORT ?? 3000
+
+mongoose.connect(MONGO_URI, {}, ()=> { console.log('connected to mongoDB')})
+mongoose.connection.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
+mongoose.connection.once("open", () => {console.log("connected to mongoose...");});
 
 //Middleware
 app.use(express.json())
